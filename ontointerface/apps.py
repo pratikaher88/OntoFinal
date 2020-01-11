fe2bfbbc448c289fdea8ec7eec07076d7f065b5d
pratikaher <pratikaher88@gmail.com>
2020-01-12 01:56:54 +0530

    FP

---
+++ ontointerface/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OntointerfaceConfig(AppConfig):
+    name = 'ontointerface'
---
+++ ontointerface/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+from owlready2 import *
+
+class SparqlQueries:
+    def __init__(self):
+        my_world = World()
+        my_world.get_ontology("/Users/pratikaher/ontologies/Owl-Ontology/trail.owl").load() #path to the owl file is given here
+        sync_reasoner(my_world)  #reasoner is started and synchronized here
+        self.graph = my_world.as_rdflib_graph()
+
+    def search(self,query):
+        resultsList = self.graph.query(query)
+        return resultsList
+
+def index(request):
+
+    query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "\
+            "ask { "\
+            "?isManagedBy rdfs:domain ?A ." \
+            "?isManagedBy rdfs:range ?B " \
+            "}"
+    
+    # query = "PREFIX owl: <http://www.w3.org/2002/07/owl#> "\
+    #         "SELECT DISTINCT ?p "\
+    #         "WHERE {?p a owl:ObjectProperty}"
+
+    runQuery = SparqlQueries()
+    response = runQuery.search(query)
+
+    resultsList= list(response)[0]
+
+    print(resultsList)
+
+
+    return HttpResponse(resultsList)
